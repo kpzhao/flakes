6523b4331dae2d0705114151d71f7b8937eca1dc
zhao <970956535@qq.com>
2023-07-13 07:01:49 +0800

    update

--- overlays/sway/default.nix
+++ overlays/sway/default.nix
@@ -1,8 +1,10 @@
-final: prev: {
+ { ... }:
+ 
+ (final: prev: {
   sway-unwrapped = prev.sway-unwrapped.overrideAttrs (self:
     {
       patches = self.patches or [ ] ++ [
-        ./0001-text_input-Implement-input-method-popups.patch
+        ./7226.patch
       ];
     });
-}
+})
