e510822b73e6bb0ac601048acbf9bdb6b57b5728
zhao <970956535@qq.com>
2023-07-13 19:40:27 +0800

    update

--- overlays/sway/default.nix
+++ overlays/sway/default.nix
@@ -1,7 +1,7 @@
  { ... }:
  
  (final: prev: {
-  sway-unwrapped = prev.sway.overrideAttrs (self:
+  sway = prev.sway.overrideAttrs (self:
     {
       patches = self.patches or [ ] ++ [
         ./7226.patch
