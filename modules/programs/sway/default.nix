43a6a2aaafada70a7271f61876c8fbec3e1c3a29
zhao <970956535@qq.com>
2023-07-16 12:16:21 +0800

    update

--- modules/programs/sway/default.nix
+++ modules/programs/sway/default.nix
@@ -34,7 +34,7 @@
 
     wayland.windowManager.sway = {
         enable = true;
-        package = pkgs.sway-hidpi;
+        # package = pkgs.sway-hidpi;
         systemd = {
             enable = true;
             xdgAutostart = true;
