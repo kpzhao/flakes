a6d06da35c7c05495b777b9cfac6db0fe67e00f4
zhao <970956535@qq.com>
2023-07-23 12:39:45 +0800

    update

--- overlays/xwayland-git.nix
+++ overlays/xwayland-git.nix
@@ -46,7 +46,8 @@ stdenv.mkDerivation rec {
   version = "git";
 
   src = fetchurl {
-    url = "mirror://xorg/individual/xserver/${pname}-23.1.2.tar.xz";
+    # url = "mirror://xorg/individual/xserver/${pname}-23.1.2.tar.xz";
+    https://xorg.freedesktop.org/archive/individual/xserver/xwayland-23.1.2.tar.xz
     sha256 = "sha256-vSXYSY7k13h0/aElEn4ts3/DMlMf68lmIx6gb66M938=";
   };
   patch = [
