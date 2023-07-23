d6b2312199325404afc98d7a28a2133b4c96ae93
zhao <970956535@qq.com>
2023-07-23 12:03:50 +0800

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,6 +3,7 @@ final: prev: {
   # we create new 'ski' attribute here!
   sway-git = final.callPackage ./sway-git.nix {};
   wlroots-git = final.callPackage ./wlroots-git.nix {};
+  xwayland-git = final.callPackage ./xwayland-git.nix {};
 
   # add more packages below:
   # ...
