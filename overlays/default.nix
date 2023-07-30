fe7965a082341c6d836d77b5519d0d0483299908
zhao <970956535@qq.com>
2023-07-30 19:16:06 +0800

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,9 @@
 final: prev: rec {
-  xwayland = prev.xwayland.overrideAttrs (_: {
-    patches = [
-      ./xwayland-hidpi.patch
-    ];
-  });
+  # xwayland = prev.xwayland.overrideAttrs (_: {
+  #   patches = [
+  #     ./xwayland-hidpi.patch
+  #   ];
+  # });
 
   wlroots_0_17 =
     (final.wlroots_0_16.overrideAttrs (old: {
@@ -15,10 +15,10 @@ final: prev: rec {
         rev = "d40bcfe2c18f057f4bc324a81230f6ba2267db44";
         hash = "sha256-6MEVZCCwmNjKgMGDBIF2J2748vJEcEOaifLycXamvz0=";
       };
-      patches = [
-        ./wlroots-0001-xwayland-support-HiDPI-scale.patch
-        ./wlroots-0002-Fix-configure_notify-event.patch
-      ];
+      # patches = [
+      #   ./wlroots-0001-xwayland-support-HiDPI-scale.patch
+      #   ./wlroots-0002-Fix-configure_notify-event.patch
+      # ];
 
       nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.hwdata];
       buildInputs =
@@ -30,48 +30,48 @@ final: prev: rec {
     }))
     .override {};
 
-  sway-test =
-    (prev.sway-unwrapped.overrideAttrs (old: {
-      version = "scene-graph-2023-07-24";
-      src = final.fetchFromGitHub {
-        owner = "Nefsen402";
-        repo = "sway";
-        rev = "ba347c525a50a5aa9639ab33de93551fb8e296b7";
-        hash = "sha256-8Omt2xfe2NY70Mt0MN2jIvhng3A1t0hD8XAIL3VGx64=";
-      };
-      patch = [
-      ];
-
-      nativeBuildInputs = with final; (old.nativeBuildInputs or []) ++ [bash-completion fish];
-
-      # Our version of sway already has this patch upstream, so we filter it out.
-      patches =
-        builtins.filter
-        (p: !p ? name || p.name != "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch")
-        (old.patches or []);
-    }))
-    .override {wlroots = final.wlroots_0_17;};
-
   # sway-test =
   #   (prev.sway-unwrapped.overrideAttrs (old: {
-  #     version = "test";
+  #     version = "scene-graph-2023-07-24";
   #     src = final.fetchFromGitHub {
-  #       owner = "swaywm";
+  #       owner = "Nefsen402";
   #       repo = "sway";
-  #       rev = "c3e6390073167bae8245d7fac9b455f9f06a5333";
-  #       sha256 = "sha256-QQOVim0U8uiAstPy9HMRBAl4McyvAdXoO44jCRlzVIQ=";
+  #       rev = "ba347c525a50a5aa9639ab33de93551fb8e296b7";
+  #       hash = "sha256-8Omt2xfe2NY70Mt0MN2jIvhng3A1t0hD8XAIL3VGx64=";
   #     };
+  #     patch = [
+  #     ];
   #
-  #     # patch = [
-  #     # ];
-  #
-  #     nativeBuildInputs = with final; (old.nativeBuildInputs or []) ++ [];
+  #     nativeBuildInputs = with final; (old.nativeBuildInputs or []) ++ [bash-completion fish];
   #
   #     # Our version of sway already has this patch upstream, so we filter it out.
   #     patches =
   #       builtins.filter
-  #       (p: !p ? name || (p.name != "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch"))
+  #       (p: !p ? name || p.name != "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch")
   #       (old.patches or []);
   #   }))
   #   .override {wlroots = final.wlroots_0_17;};
+
+  sway-test =
+    (prev.sway-unwrapped.overrideAttrs (old: {
+      version = "test";
+      src = final.fetchFromGitHub {
+        owner = "swaywm";
+        repo = "sway";
+        rev = "c3e6390073167bae8245d7fac9b455f9f06a5333";
+        sha256 = "sha256-QQOVim0U8uiAstPy9HMRBAl4McyvAdXoO44jCRlzVIQ=";
+      };
+
+      # patch = [
+      # ];
+
+      nativeBuildInputs = with final; (old.nativeBuildInputs or []) ++ [];
+
+      # Our version of sway already has this patch upstream, so we filter it out.
+      patches =
+        builtins.filter
+        (p: !p ? name || (p.name != "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch"))
+        (old.patches or []);
+    }))
+    .override {wlroots = final.wlroots_0_17;};
 }
