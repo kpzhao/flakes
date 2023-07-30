652b39e68d2ff367e3b708a451f1bbb6764646c1
zhao <970956535@qq.com>
2023-07-30 20:24:43 +0800

    update

--- host/configuration.nix
+++ host/configuration.nix
@@ -127,6 +127,7 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     neofetch
+    telegram-desktop
     element-desktop
     firefox
     gcc
