fc6677c7a6035a5f3237f44d88672ce1de4c0ece
zhao <970956535@qq.com>
2023-08-06 08:21:45 +0800

    update

--- pkgs/wallpaper-blur.nix
+++
@@ -1,4 +0,0 @@
-{ runCommand, wallpaper, imagemagick }:
-runCommand "wallpaper-blur.jpg" { } ''
-  ${imagemagick}/bin/convert -blur 14x5 ${wallpaper} $out
-''
--- pkgs/wallpaper.nix
+++
@@ -1,6 +0,0 @@
-{ fetchurl }:
-fetchurl {
-  name = "wallpaper.jpg";
-  url = "https://pbs.twimg.com/media/E9irhxhVUAUaBCr?format=jpg";
-  hash = "sha256-Rhjj1K0FXKGzKswoLj1H0Yi/QHswzPcGW6aLMiekURA=";
-}
